a83f0017aa19441ad7e3242289ebddd4cc6159c6
Felipe Moura <felipemimoura@gmail.com>
2022-07-13 18:07:06 -0300

    feat: create endpoint to create new account

--- capter_I/primeiro_projeto/src/index.js
+++ capter_I/primeiro_projeto/src/index.js
@@ -1,9 +1,29 @@
 const express = require("express")
+const { v4: uuidV4 } = require('uuid')
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body
+    const id = uuidV4()
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send()
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
 })
